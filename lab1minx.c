e2b2b46b8d9b9481d12016ba78b77a285219fd78
Natwasa Paksrikul 6604062630196_Sec1 <152253951+6604062630196@users.noreply.github.com>
2024-11-26 14:47:47 +0700

    Add files via upload

---
+++ lab1minx.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+	int n1,n2,r;
+	n1 = 20;
+	n2 = 30;
+	r = 
+	if (n1>n2) {
+		printf("n1 is greater than n2");
+	}else{
+		printf("n2 is greather than n1");
+	}
+	
+}
